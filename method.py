7d71a6e3b229917d5f8843354201e86df1a21697
Han <101443278+Handra1@users.noreply.github.com>
2022-03-14 15:14:35 +0700

    Add files via upload

---
+++ method.py
@@ -0,0 +1,8 @@
+kel = ['saya', 5, 'me', 2, 'anda', 3]
+print(kel.index('me'))
+print(kel.count(5))
+kel.append(11)
+print(kel)
+adik = 'anda'
+print(adik.capitalize())
+print(adik.replace('a', 'ra'))
